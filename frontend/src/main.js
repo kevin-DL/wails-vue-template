4eec366672d570c21a3fc0b83ded80918f5c19f2
kevin-DL <kevanatole@gmail.com>
2024-04-26 14:31:58 +0100

    Add vue-router Add home page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import App from './App.vue'
 import './style.css';
 
-createApp(App).mount('#app')
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		component: () => import('./pages/Index.vue')
+	}
+]
+
+const router = createRouter({
+	history: createWebHashHistory(),	
+	routes
+})
+
+
+createApp(App)
+	.use(router)
+	.mount('#app')
